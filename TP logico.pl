e852fa5662c236461bebd402438dc0282d2773cc
Unknown <nacho.feder@gmail.com>
2017-06-15 19:03:23 -0300

    Primer entrega realizada

---
+++ TP logico.pl
@@ -0,0 +1,11 @@
+programador(fernando, cobol).
+programador(fernando, visualBasic).
+programador(fernando, java).
+programador(julieta, java).
+programador(marcos, java).
+programador(santiago, ecmaScript).
+programador(santiago, java).
+
+rol(fernando, analistaFuncional).
+rol(andres, projectLeader).
+rol(Persona, programador):- programador(Persona, _).
